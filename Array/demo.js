833387415b3220c03d535a6f2eeae47f717e70fa
xzh <770158999@qq.com>
2016-12-17 15:44:44 +0800

    add List

--- Array/demo.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by xzh on 2016/12/15.
- */
-let a ='1';
-console.log(a.charCodeAt());
-console.log(String.fromCharCode(26));
--- Array/matrix.js
+++ Array/matrix.js
@@ -3,13 +3,15 @@
  */
 //创建一个数组用于保存字母 输入单词返回数组项
 let Matrix = function (rows, cows, fn) {
-    let arr = [];
+    let arr= [];
+    arr.rows = rows;
+    arr.cows = cows;
     for (let i = 0; i < rows; i++) {
         let row = [];
         for (let j = 0; j < cows; j++) {
             if (typeof fn === 'function') {
                 try {
-                    if (fn(i, j)) {
+                    if (fn(i, j)||fn(i, j)===0) {
                         row[j] = fn(i, j)
                     }
                 } catch (e) {
@@ -23,30 +25,87 @@ let Matrix = function (rows, cows, fn) {
     }
     return arr;
 };
+function MatrixUtils(){
+    //矩阵倒置 [[A,a],[B,b]...[Z,z]]
+    this.inverse= function (matrix) {
+        if(matrix.rows&&matrix.cows){
+            let arr=[];
+            arr.rows=matrix.cows;
+            arr.cows=matrix.rows;
+            for(let i=0;i<matrix.cows;i++){
+                let tmp = [];
+                for(let j=0;j<matrix.rows;j++){
+                    tmp[j]=matrix[j][i];
+                }
+                arr[i]=tmp;
+            }
+            return arr;
+        }else{
+            throw new Error('invalid matrix');
+        }
+    };
+    //仅限数字型
+    this.m_add = function (matrixA,matrixB) {
+        let arr =[];
+        arr.rows = matrixA.rows<matrixB.rows?matrixB.rows:matrixA.rows;
+        arr.cows = matrixA.cows<matrixB.cows?matrixB.cows:matrixA.cows;
+        for (let i=0;i<arr.rows;i++){
+            let tmp=[];
+            for(let j=0;j<arr.cows;j++){
+                tmp[j]=(matrixA[i][j]||0) + (matrixB[i][j]||0);
+            }
+            arr[i]=tmp;
+        }
+        return arr;
+    }
+}
+let utils = new MatrixUtils;
+// let abc = function (rows, cows) {
+//
+//     switch (rows) {
+//         case 0:
+//             if (cows<10) {
+//                 return String.fromCharCode(48 + cows);
+//             }
+//             break;
+//         case 1:
+//             if (cows < 26) {
+//                 return String.fromCharCode(65 + cows);
+//             }
+//             break;
+//         case 2:
+//             if (cows < 26) {
+//                 return String.fromCharCode(97 + cows);
+//             }
+//             break;
+//     }
+// };
 let abc = function (rows, cows) {
 
     switch (rows) {
         case 0:
-            if (cows<10) {
-                return String.fromCharCode(48 + cows);
-            }
-            break;
-        case 1:
             if (cows < 26) {
                 return String.fromCharCode(65 + cows);
             }
             break;
-        case 2:
+        case 1:
             if (cows < 26) {
                 return String.fromCharCode(97 + cows);
             }
             break;
+        default:
+            break;
     }
 };
-console.log(Matrix(3, 28,abc));
-let abc_matrix = Matrix(3, 26,abc);
-console.log(abc_matrix[1][5]);//>>>F
-//矩阵倒置
-Matrix.prototype.inverse = function () {
-
-};
+let abc_matrix =Matrix(2, 26,abc);
+// console.log(abc_matrix);//[[A,B...Z],[a,b...z]]
+// console.log(abc_matrix[0][5]);//>>>F
+// console.log(abc_matrix.rows);// 2
+let cba_matrix = utils.inverse(abc_matrix);
+// console.log(cba_matrix);
+let matrixA =Matrix(4,5,function (a,b) {
+    return a+b;
+});
+console.log(matrixA);
+let out =utils.m_add(matrixA,matrixA);
+console.log(out);
