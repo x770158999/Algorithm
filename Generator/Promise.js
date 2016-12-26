92d687142469b5e5ee9853f547c296fd888494ac
xzh <770158999@qq.com>
2016-12-26 11:26:22 +0800

    add linkdeList，HashTable

---
+++ Generator/Promise.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/12/25.
+ */
+const Promise = function () {
+
+};
+
+
+//用于异步执行onFulfilled/onRejected
+const asyncCall = process.nextTick;
+
+//promise对象，X为入参
+function resolve(promise,x) {
+
+}
